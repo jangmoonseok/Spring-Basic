de1863557e535d5fe9d504fe700ef1791e777929
DESKTOP-BP73NMV\PC-25 <jmh4228@gmail.com>
2022-03-23 21:03:11 +0900

    빈 스코프, 콜백

---
+++ hello/src/test/java/com/example/demo/autowired/AllBeanTest.java
@@ -0,0 +1,54 @@
+package com.example.demo.autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.demo.AutoAppConfig;
+import com.example.demo.autowired.AllBeanTest.DiscountService;
+import com.example.demo.discount.DiscountPolicy;
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+
+public class AllBeanTest {
+	@Test
+	void findAllBean() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+		
+		DiscountService discountService = ac.getBean(DiscountService.class);
+		Member member = new Member(1L, "userA", Grade.VIP);
+		int fixDiscountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+		assertThat(fixDiscountPrice).isEqualTo(1000);
+		
+		int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+		assertThat(rateDiscountPrice).isEqualTo(2000);
+	}
+	
+	static class DiscountService{
+		private final Map<String, DiscountPolicy> policyMap;
+		private final List<DiscountPolicy> policyList;
+		
+		public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+			super();
+			this.policyMap = policyMap;
+			this.policyList = policyList;
+			System.out.println("policyMap : " + policyMap);
+			System.out.println("policyList : " + policyList);
+		}
+
+		public int discount(Member member, int price, String discountCode) {
+			DiscountPolicy discountPolicy = policyMap.get(discountCode);
+			
+			return discountPolicy.discount(member, price);
+		}
+		
+		
+		
+		
+	}
+}
---
+++ hello/src/test/java/com/example/demo/scope/SingletonTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import com.example.demo.scope.SingletonTest.SingletonBean;
+
+public class SingletonTest {
+	@Test
+	void singletonBeanFind() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+		
+		SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+		SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+		System.out.println("singletonBean1 = " + singletonBean1);
+		System.out.println("singletonBean2 = " + singletonBean2);
+		
+		assertThat(singletonBean1).isSameAs(singletonBean2);
+		
+		ac.close();
+	}
+	
+	@Scope("singleton")
+	static class SingletonBean{
+		@PostConstruct
+		public void init() {
+			System.out.println("init");
+		}
+		
+		@PreDestroy
+		public void destroy() {
+			System.out.println("destroy");
+		}
+	}
+}
